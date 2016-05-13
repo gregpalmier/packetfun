3979bb78928bc70295a09829b1fdbe71ec531ff7
Greg Palmier <gregpalmier@gmail.com>
2016-05-12 20:18:13 -0500

    ip_saddr

--- ip_saddr.rb
+++ ip_saddr.rb
@@ -3,12 +3,14 @@ require 'packetfu'
 require 'pcaprub'
 require 'pry'
 
+# basic source ip header manipulation.
+
 # http://www.rubydoc.info/github/todb/packetfu/PacketFu/Config
 config = PacketFu::Config.new(PacketFu::Utils.whoami?(:iface=> 'en0')).config
 # pkt = PacketFu::TCPPacket.new(:config => $config , :flavor => 'Linux')
 pkt = PacketFu::TCPPacket.new(:config => $config)
 
-pkt.ip_saddr = '2.2.2.2'
+pkt.ip_saddr = '192.168.1.199'
 pkt.ip_daddr = '192.168.1.15'
 
 # binding.pry
