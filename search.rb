3143861285ee926d13ae09c0ea3d2098b1123457
Greg Palmier <gregpalmier@gmail.com>
2016-06-11 17:07:29 -0500

    search

--- search.rb
+++ search.rb
@@ -6,14 +6,26 @@ require 'awesome_print'
 require 'shodan'
 require 'pry'
 require 'yaml'
+require 'csv'
 
 shodan = YAML.load_file(File.join(File.dirname(__FILE__), 'shodan.yml'))
 api = Shodan::Shodan.new(shodan['key'])
 
 term = ARGV.first
+# paginate?
 result = api.search(term)
-# binding.pry
-result['matches'].each do |host|
-  puts "IP: #{host['ip_str']}"
-  host['hostnames'].each { |x| puts x }
+CSV.open("#{term}.csv", 'w') do |csv|
+  csv << ['hostnames', 'ip_str', 'ports']
+  result['matches'].each do |host|
+    h = api.host(host['ip_str'])
+    hosts = []
+    unless h['hostnames'].empty?
+      hosts << h['hostnames']
+    else
+      hosts = ['None']
+    end
+    csv << [ hosts.join(','), h['ip_str'], h['ports'].join(',')]
+    # don't get throttled?
+    sleep 1
+  end
 end
